38ea1cb0c3b21091e1c60ae98ff865ea2f1754c5
Harsh260504 <madhaavi972@gmail.com>
2026-05-19 15:22:58 +0530

    chore: clean up empty code change sections in the changes log

--- harshportfolio/src/App.jsx
+++ harshportfolio/src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  
-  <>
-  <h1 className='bg-pink-300 text-3xl'> welcome to harsh's portfolio</h1>
-  </>
-  )
+    <>
+      <h1 className="bg-pink-300 text-3xl"> welcome to harsh's portfolio</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
